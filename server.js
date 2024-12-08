46cac649e12456b36ab9bdad1176e85a2e636ede
subham007-coder <dsubham109@gmail.com>
2024-12-08 10:53:45 +0530

    add

--- server.js
+++ server.js
@@ -72,6 +72,6 @@ app.get('/', (req, res) => {
 
 // Start Server and listen on all interfaces
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(5000, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
